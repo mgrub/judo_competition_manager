6641f02f98b5d66b59a895720049be069328e3ec
Maximilian Gruber <m.gruber@mailbox.org>
2020-03-01 09:40:17 +0100

    refactor(modes): change handling of initial fights

--- modes.py
+++ modes.py
@@ -4,13 +4,30 @@ import random
 
 class ModeTemplate():
 
+    initial_known_fights = {}  # {<local_fight_id> : (competitor_1_local_id, competitor_2_local_id)}
+
     # general functions, valid for all modes
     def __init__(self, session, group):
         self.session = session
         self.group = group
 
     def init_fights(self):
-        self.session.add_all(self._fights())
+        # create fights
+        n_fights = self.get_total_number_of_fights()
+        fights = []
+
+        # populate fight with competitors, if it is one of the initial known fights
+        for local_fight_id in range(n_fights):
+            if local_fight_id in self.initial_known_fights:
+                c1 = self.get_competitor_from(self.initial_known_fights[local_fight_id][0])
+                c2 = self.get_competitor_from(self.initial_known_fights[local_fight_id][1])
+                fight = Fight(local_id = local_fight_id, competitor_1 = c1, competitor_2 = c2, group = self.group)
+            else:
+                fight = Fight(local_id = local_fight_id, competitor_1 = None, competitor_2 = None, group = self.group)
+            fights.append(fight)
+
+        # write to session
+        self.session.add_all(fights)
         self.session.commit()
 
     def delete_fights(self):
@@ -40,8 +57,8 @@ class ModeTemplate():
     def set_winner(self, local_fight_id, local_competitor_id, points, subpoints):
 
         # resolve local ids
-        fight = self._get_fight_from(local_fight_id)
-        competitor = self._get_competitor_from(local_competitor_id)
+        fight = self.get_fight_from(local_fight_id)
+        competitor = self.get_competitor_from(local_competitor_id)
 
         # set winner
         fight.winner = competitor
@@ -58,19 +75,23 @@ class ModeTemplate():
             if fight.winner != None:
                 self.propagate_fight_outcome(fight.local_id)
 
-    def _get_fight_from(self, local_fight_id):
+    def get_fight_from(self, local_fight_id):
         return self.session.query(Fight).filter(and_(Fight.group==self.group, Fight.local_id==local_fight_id)).first()
 
-    def _get_competitor_from(self, local_competitor_id):
+    def get_competitor_from(self, local_competitor_id):
         gca = self.session.query(GroupCompetitorAssociation).filter(and_(GroupCompetitorAssociation.group==self.group, GroupCompetitorAssociation.local_lot==local_competitor_id)).first()
-        return gca.competitor
+        if gca == None:
+            return None
+        else:
+            return gca.competitor
 
     def get_number_of_competitors(self):
         return len(self.group.competitors)
 
+    def get_total_number_of_fights(self):
         raise NotImplementedError()
 
-    def _fights(self):
+    def propagate_fight_outcome(self, local_fight_id):
         raise NotImplementedError()
 
     def evaluate_group_result(self):
@@ -83,11 +104,14 @@ class KoModeTemplate(ModeTemplate):
     # need to be set
     fight_topology = {}  # {local_fight_id : (winner_next_fight , loser_next_fight)}   and   winner_next_fight = (<local_id>, <as_competitor>)
     fight_evaluation = {}  # {local_fight_id : winner_has_place}
+    
+    def get_total_number_of_fights(self):
+        return len(self.fight_topology)
 
     def propagate_fight_outcome(self, local_fight_id):
         
         # get fight, winner and loser
-        fight = self._get_fight_from(local_fight_id)
+        fight = self.get_fight_from(local_fight_id)
         winner = fight.winner
         if winner == fight.competitor_1:
             loser = fight.competitor_2
@@ -99,15 +123,15 @@ class KoModeTemplate(ModeTemplate):
 
         # if there is a next fight for the winner, set as competitor there
         if next_fight_winner:
-            next_fight = self._get_fight_from(next_fight_winner["local_id"])
-            if next_fight_winner["as_competitor"] == 1:
+            next_fight = self.get_fight_from(next_fight_winner[0])
+            if next_fight_winner[1] == 1:
                 next_fight.competitor_1 = winner
             else:
                 next_fight.competitor_2 = winner
 
         # if there is a next fight for the loser, set as competitor there
         if next_fight_loser:
-            next_fight = self._get_fight_from(next_fight_loser[0])
+            next_fight = self.get_fight_from(next_fight_loser[0])
             if next_fight_loser[1] == 1:
                 next_fight.competitor_1 = loser
             else:
@@ -141,19 +165,9 @@ class ko_full_repechage_8(KoModeTemplate):
         10 : (None, None),
     }
 
-    def _fights(self):
-        
-        fights = []
-        fights.append(Fight(local_id =  0, competitor_1 = 0, competitor_2 = 1, group = self.group))
-        fights.append(Fight(local_id =  1, competitor_1 = 2, competitor_2 = 3, group = self.group))
-        fights.append(Fight(local_id =  2, competitor_1 = 4, competitor_2 = 5, group = self.group))
-        fights.append(Fight(local_id =  3, competitor_1 = 6, competitor_2 = 7, group = self.group))
-        fights.append(Fight(local_id =  4, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id =  5, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id =  6, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id =  7, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id =  8, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id =  9, competitor_1 = None, competitor_2 = None, group = self.group))
-        fights.append(Fight(local_id = 10, competitor_1 = None, competitor_2 = None, group = self.group))
-
-        return fights
+    initial_known_fights = {
+        0: (0, 1),
+        1: (2, 3),
+        2: (4, 5),
+        3: (6, 7),
+    }
