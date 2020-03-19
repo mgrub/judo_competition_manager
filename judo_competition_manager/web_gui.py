acf3b36fe07bd6c638ef2004ae40d4e241ff9dda
Maximilian Gruber <m.gruber@mailbox.org>
2020-03-19 20:59:26 +0100

    change winner in DB from dropdown

--- judo_competition_manager/web_gui.py
+++ judo_competition_manager/web_gui.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from judo_competition_manager.models import Group
 from judo_competition_manager.database import init_db, db_session
@@ -9,10 +9,20 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
-@app.route('/group/<int:group_id>')
+@app.route('/group/<int:group_id>', methods=["GET", "POST"])
 def group_overview(group_id):
     g = Group.query.filter(Group.id == group_id).first()
+
+    # change the winner
+    if request.method == "POST":
+        tmp = request.form["winner_select"]
+        fight_id, winner_id = tmp.split(".")
+        g.load_mode_class(g.mode, db_session)
+        g.mode_class.set_winner(fight_id, winner_id, 1, 10, local_ids=False)
+
+    # always return the group overview
     return render_template("group_overview.html", g=g)
+    
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
