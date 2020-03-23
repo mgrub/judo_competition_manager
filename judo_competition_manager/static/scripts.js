22f9544f4396dbb928e0ea2a2e37c3e0a896d829
Maximilian Gruber <m.gruber@mailbox.org>
2020-03-23 21:40:16 +0100

    prepare autocomplete for competitor selection

--- judo_competition_manager/static/scripts.js
+++ judo_competition_manager/static/scripts.js
@@ -16,3 +16,15 @@ function fight_set_winner(fight_id, winner_id, points, subpoints){
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send("winner_id=" + winner_id.toString() +"&points=" + points.toString() + "&subpoints=" + subpoints.toString());
 }
+
+function competitor_autocomplete(list_id, search_text){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(list_id).innerHTML = this.responseText
+        }
+    };
+    xhr.open('POST', '/query');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("competitors_matching=" + search_text);
+}
--- judo_competition_manager/web_gui.py
+++ judo_competition_manager/web_gui.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
 
-from judo_competition_manager.models import Group, Fight
+from judo_competition_manager.models import Group, Fight, Competitor
 from judo_competition_manager.database import init_db, db_session
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def index():
+    groups = Group.query.all()
+    return render_template("index.html", groups=groups)
 
-@app.route('/group/<int:group_id>', methods=["GET", "POST"])
+@app.route('/group/<int:group_id>')
 def group_overview(group_id):
     g = Group.query.filter(Group.id == group_id).first()
 
@@ -27,9 +28,6 @@ def fight_set_winner(fight_id):
     f = Fight.query.filter(Fight.id == fight_id).first()
     g = f.group
 
-    for key, val in request.form.items():
-        print(key, val)
-
     # TODO: make consistency/plausability checks
     winner_id = request.form.get("winner_id")
     points = request.form.get("points")
@@ -40,6 +38,27 @@ def fight_set_winner(fight_id):
     g.mode_class.set_winner(fight_id, winner_id, points, subpoints, local_ids=False)
     
     return '', 204  # no page
+
+@app.route('/query', methods=["GET", "POST"])
+def query():
+    matching_competitors = []
+
+    if request.method == "GET":
+        request_data = request.args
+    else:
+        request_data = request.form
+
+    if "competitors_matching" in request_data:
+        # match all names and firstnames starting with the request search term (ilike -> case insensitive)
+        search = request_data.get("competitors_matching") + "%"
+        matches_by_name = Competitor.query.filter(Competitor.name.ilike(search)).limit(5).all()
+        matches_by_firstname = Competitor.query.filter(Competitor.firstname.ilike(search)).limit(5).all()
+
+        matching_competitors = matches_by_name + matches_by_firstname
+
+        return render_template("competitor_autocomplete.html", list_of_competitors=matching_competitors)
+    else:
+        return ""
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
