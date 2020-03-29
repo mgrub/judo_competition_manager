8d00aec56369671348a44a32d8e28349ec4c7ffc
Maximilian Gruber <m.gruber@mailbox.org>
2020-03-29 12:26:28 +0200

    remove unused import

--- db/database.py
+++ db/database.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import and_, or_
 
-import json
-
 # define connectors
 engine = create_engine("sqlite:///configuration.db", echo=False)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
